67b47d19630d83d81437e5f113a04dd2e7612a73
Matthew Adlam <mattadlam3@gmail.com>
2020-02-18 15:09:17 +1300

    New files and PHP change

---
+++ computinghome.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css" type="text/css" charset="utf-8">
+    <script>window.addEventListener("keydown", function(e) {
+      // space, page up, page down and arrow keys:
+      if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+          e.preventDefault();
+      }
+  }, false);</script>
+    
+    <title>Document</title>
+</head>
+   <header>
+       <br>
+       <a href = "index.php" class = "header-text">Study Time!</a> 
+    </header>
+    <div class="bodybox">
+        <div class="gridcontainer">
+        <div class="item1">
+            <h1>HTML!</h1>
+        </div>
+        <div class="item2">
+            <h1>CSS!</h1>
+        </div>
+        <div class="item2-1">
+            <h1>PHP!</h1>
+        </div>
+        <div class="item2-2">
+            <h1>C#.net!</h1>
+        </div>
+       </div>
+       </div>
+</body>
+</html>
