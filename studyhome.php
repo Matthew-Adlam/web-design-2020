703f13d4ae6ca72d395f8a4eee75b84e97dda71f
Matthew Adlam <mattadlam3@gmail.com>
2020-05-22 12:27:00 +1200

    favicon update

--- studyhome.php
+++ studyhome.php
@@ -5,12 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css" type="text/css" charset="utf-8">
-    <script>window.addEventListener("keydown", function(e) {
-      // space, page up, page down and arrow keys:
-      if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-          e.preventDefault();
-      }
-  }, false);</script>
+    <link rel="icon" href="images/favicon.ico">
+
     
     <title>Document</title>
 </head>
