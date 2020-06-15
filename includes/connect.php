37c659c89ab84b7ed802ec263a0d3a7ecb2631e5
Matthew Adlam <mattadlam3@gmail.com>
2020-06-15 12:53:19 +1200

    Delete file with password
     in order to make this repo public

--- includes/connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=matthewadlam_assessment2020','matthewadlam','sBmP89XfKn7GFg');
-}
-    catch(PDOException $e) {
-        echo 'Unable to connect to the databse server.';
-        exit;
-    }
-?>
